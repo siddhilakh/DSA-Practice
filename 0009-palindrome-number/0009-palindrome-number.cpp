0367c97f9ec26528c339799b60dc974a6155b218
siddhilakh <siddhilakh@gmail.com>
2026-06-12 23:50:44 +0530

    Time: 7 ms (14.96%), Space: 8.5 MB (65.24%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+    int dup = x;
+    long long revNum = 0;
+    while (x > 0) {
+        int ld = x % 10;
+        revNum = (revNum*10) + ld;
+        x = x/10;
+    }
+    return dup == revNum;
+        
+    }
+};
